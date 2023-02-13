a57f56a560b733c0b5c00937a2b4e93fcad3e64c
reactqasim123 <hellyah313@gmail.com>
2023-02-13 20:44:50 +0500

    qasim

--- src/components/Navbar/Drawer/LogoutButton.jsx
+++ src/components/Navbar/Drawer/LogoutButton.jsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+import Button  from '@mui/material/Button';
+import {useTheme } from "@mui/material";
+
+import { tokens } from "../../../theme";
 
 export default function LogoutButton() {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+  
   return (
-      <Button variant="contained">Logout</Button>
+      <Button sx={{ color: colors.greenAccent[600] }} variant="contained">Logout</Button>
   );
 }
